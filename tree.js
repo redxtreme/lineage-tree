1adc2e66ca6466767bb6385fbdfbada7f22be753
Zach Hopper <redxtreme232@gmail.com>
2017-11-10 16:29:12 -0600

    broke up by line return

--- tree.js
+++ tree.js
@@ -1,18 +1,58 @@
-read
-function readTextFile(file)
-{
+getLineData('lines.txt')
+var lineText = '';
+//reference vid https://www.youtube.com/watch?v=ZZncFax8yNY
+
+/* Gets the line data from the file */
+function getLineData(file) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
                 var allText = rawFile.responseText;
-                alert(allText);
+                lineText = allText;
+                handleText();
+                console.log('here');
             }
         }
     }
     rawFile.send(null);
+}
+
+function errorHandler(event) {
+    if (evt.target.error.name == 'NotReadableError')
+ {
+     alert('Cannot read file!');
+ }
+}
+
+function handleText() {
+    console.log(lineText.split('\n'));
+}
+
+function getAsText(fileToRead) {
+    var reader = new FileReader();
+    
+    reader.readAsText(fileToRead);
+    
+    reader.onload = loadHandler;
+    reader.onerror = errorHandler;
+}
+
+function loadHandler(event) {
+    var csv = event.target.result;
+    processData(csv);
+}
+
+function processData(csv) {
+    var allTextLines = csv.split('/\r\n|\n');
+}
+
+function handleFiles(files) {
+    if (window.FileReader) {
+        getAsText(file);
+        fileLoaded = true;
+    } else  {
+        alert('File reader not supported in browser');
+    }
 }
