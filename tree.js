a06efcdd0f0749c48189282ee38d1022be553c38
Zach Hopper <redxtreme232@gmail.com>
2017-11-10 16:48:55 -0600

    removed extra code

--- tree.js
+++ tree.js
@@ -1,6 +1,7 @@
 getLineData('lines.txt')
 var lineText = '';
 //reference vid https://www.youtube.com/watch?v=ZZncFax8yNY
+// also here https://www.html5rocks.com/en/tutorials/file/dndfiles/
 
 /* Gets the line data from the file */
 function getLineData(file) {
@@ -19,40 +20,6 @@ function getLineData(file) {
     rawFile.send(null);
 }
 
-function errorHandler(event) {
-    if (evt.target.error.name == 'NotReadableError')
- {
-     alert('Cannot read file!');
- }
-}
-
 function handleText() {
     console.log(lineText.split('\n'));
-}
-
-function getAsText(fileToRead) {
-    var reader = new FileReader();
-    
-    reader.readAsText(fileToRead);
-    
-    reader.onload = loadHandler;
-    reader.onerror = errorHandler;
-}
-
-function loadHandler(event) {
-    var csv = event.target.result;
-    processData(csv);
-}
-
-function processData(csv) {
-    var allTextLines = csv.split('/\r\n|\n');
-}
-
-function handleFiles(files) {
-    if (window.FileReader) {
-        getAsText(file);
-        fileLoaded = true;
-    } else  {
-        alert('File reader not supported in browser');
-    }
 }
