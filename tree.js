798d05666d7cc8cf0c574dca089a76cabcbffb90
Zach Hopper <redxtreme232@gmail.com>
2017-11-10 17:04:02 -0600

    broke lines up individually

--- tree.js
+++ tree.js
@@ -1,5 +1,5 @@
-getLineData('lines.txt')
-var lineText = '';
+getLineData('lines.txt');
+
 //reference vid https://www.youtube.com/watch?v=ZZncFax8yNY
 // also here https://www.html5rocks.com/en/tutorials/file/dndfiles/
 
@@ -11,15 +11,18 @@ function getLineData(file) {
         if (rawFile.readyState === 4) {
             if (rawFile.status === 200 || rawFile.status == 0) {
                 var allText = rawFile.responseText;
-                lineText = allText;
-                handleText();
-                console.log('here');
+                handleText(allText);
             }
         }
     }
     rawFile.send(null);
 }
 
-function handleText() {
-    console.log(lineText.split('\n'));
+function handleText(allText) {
+    var lines = allText.split('\n');
+    
+    for (var lineIndex in lines) {
+        var line = lines[lineIndex];
+        console.log(line.split(' '));
+    }
 }
