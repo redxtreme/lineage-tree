00e0a0ba79a8b0f68dd705f8c01eb7a15cde3199
Zach Hopper <redxtreme232@gmail.com>
2017-11-10 18:15:59 -0600

    trying to store parents

--- tree.js
+++ tree.js
@@ -3,7 +3,7 @@ getLineData('lines.txt');
 //reference vid https://www.youtube.com/watch?v=ZZncFax8yNY
 // also here https://www.html5rocks.com/en/tutorials/file/dndfiles/
 
-/* Gets the line data from the file */
+// Gets the line data from the file
 function getLineData(file) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
@@ -18,11 +18,41 @@ function getLineData(file) {
     rawFile.send(null);
 }
 
+//imports and formats the text file
 function handleText(allText) {
+    function Node(parent = null, id = null, name = null) {
+        this.parent = parent;
+        this.id = id;
+        this.name = name;
+    };
+    var nodes = [];
+
+    //break text up line by line
     var lines = allText.split('\n');
-    
+
+    //loop through every person's line
     for (var lineIndex in lines) {
-        var line = lines[lineIndex];
-        console.log(line.split(' '));
+
+        //isolate each id number
+        var line = lines[lineIndex].split(' ');
+
+        //loop through every id in a line
+        for (var pIndex in line) {
+            var newNode = new Node();
+
+            newNode.id = line[pIndex];
+
+            //if this is not the last node in a line
+            if (pIndex !== line.length - 2) {
+                newNode.parent = line[pIndex + 1];
+            }
+
+            nodes.push(newNode);
+        }
     }
+    print(nodes);
+}
+
+function print(stuff) {
+    console.log(stuff);
 }
